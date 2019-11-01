42f47f17c742bef457c793b23e43fcf83cea969b
zoe-gonzales <zoegonzales03@gmail.com>
2019-10-31 20:43:31 -0600

    importing only used bootstrap groups

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import Heading from './components/Heading';
+import NextBtn from './components/Button';
+import RSVPButton from './components/RSVPButton';
+import SignIn from './components/SignInForm';
+import SignUp from './components/SignUpForm';
 
 function App() {
-  return <Heading />
+  return (
+    <div>
+      <Heading />
+      <NextBtn>Go</NextBtn>
+      <RSVPButton>Going</RSVPButton>
+      <SignIn />
+      <SignUp />
+    </div>
+  )
 }
 
 export default App;
--- client/src/components/RSVPButton.jsx
+++ client/src/components/RSVPButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ children, color }) => {
-    return <Btn type="button" className="oval-btn btn btn-info" style={{ backgroundColor: color, border: '1px solid ' + color }}>{children}</Btn>
+    return <button type="button" className="oval-btn btn btn-info" style={{ backgroundColor: color, border: '1px solid ' + color }}>{children}</button>
 }
 
 export default Button;

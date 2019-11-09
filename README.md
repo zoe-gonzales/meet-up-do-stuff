eea27d77f8359e387fa3109048421a10426b54af
zoe-gonzales <zoegonzales03@gmail.com>
2019-11-09 16:44:06 -0700

    update readme, add structure for routes

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ This project is a basic clone of the site [Meetup.com](https://www.meetup.com/),
 
 I chose this project because I wanted to create a full stack app using [Go](https://golang.org/) on the back end and [React](https://reactjs.org/) on the front end.
 
-It was also really important to me that I wrote unit tests for as many of the functions in the code as possible. Tests for all exported functions are under ` /test ` and tests for unexported functions can be found within their own packages. I will soon be adding tests for the front end as well.
-
 Other tools/packages used so far include: <br>
 [PostgreSQL](https://www.postgresql.org/) <br>
 [GORM](https://gorm.io/) <br>
--- api/routes.go
+++ api/routes.go
@@ -9,8 +9,7 @@ import (
 	"github.com/zoe-gonzales/meet-up-do-stuff/user"
 )
 
-// AuthenticateUser authenticates the user and logs them in if they have submitted
-// correct email and passwords
+// AuthenticateUser authenticates the user and logs them in correct credentials are provided
 func AuthenticateUser(w http.ResponseWriter, r *http.Request) {}
 
 // RegisterNewUser creates a new user in the db with provided credentials
@@ -30,3 +29,27 @@ func RegisterNewUser(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "An error occurred with your registration.")
 	}
 }
+
+// UpdateUserDetails edits and saves user email or password
+func UpdateUserDetails(w http.ResponseWriter, r *http.Request) {}
+
+// DeleteUser deletes a user by id
+func DeleteUser(w http.ResponseWriter, r *http.Request) {}
+
+// UpdateProfile edits and saves details of a user's profile
+func UpdateProfile(w http.ResponseWriter, r *http.Request) {}
+
+// GetAllEvents retrieves all events
+func GetAllEvents(w http.ResponseWriter, r *http.Request) {}
+
+// GetSingleEvent retrieves event by id
+func GetSingleEvent(w http.ResponseWriter, r *http.Request) {}
+
+// AddEvent posts a new event
+func AddEvent(w http.ResponseWriter, r *http.Request) {}
+
+// UpdateEvent edits and saves existing event data
+func UpdateEvent(w http.ResponseWriter, r *http.Request) {}
+
+// DeleteEvent deletes an event by id
+func DeleteEvent(w http.ResponseWriter, r *http.Request) {}

e92131ed55c537ba31e62ae909652379742f6dfc
zoe-gonzales <zoegonzales03@gmail.com>
2019-12-22 11:52:12 -0700

    validation for sign up form

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -2,34 +2,71 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ContentContainer from "./ContentContainer";
 import Button from './Button';
+import Alert from '../components/ValidationAlert';
 import UseForm from '../hooks/UseForm';
+import UseValidator from '../hooks/UseValidator';
 import API from '../utils/API';
+import validate from '../utils/validate';
 
 const SignUpForm = () => {
+   const {
+        validInputs,
+        invalidateInputs,
+   } = UseValidator();
+
    const { inputs, handleInputChange, handleSubmit } = UseForm(() => {
-     const { username, password } = inputs;
+     const { username, password, confirmPassword } = inputs;
      const body = {
         Email: username,
         Password: password,
      }
-     API
-       .signUpUser(body)
-       .then(res => console.log(res))
-       .catch(err => console.log(err));
+
+     if (
+        validate.email(body.Email) &&
+        validate.password(body.Password) &&
+        validate.match(body.Password, confirmPassword)
+     ) {
+        API
+        .signUpUser(body)
+        .then(res => console.log(res))
+        .catch(err => {
+            console.log(err)
+            alert("The email address you're using already exists. Please try logging in instead.")
+        });
+     } else {
+         invalidateInputs()
+     }
+
    }, 'auth');
 
     return (
         <ContentContainer color="white">
+            {validInputs ? null : <Alert>Some of your information doesn't meet the requirements. Please check that you have submitted a valid email and password and that your passwords match.</Alert>}
             <h4 className="title text-center">sign up</h4>
             <form onSubmit={e => handleSubmit(e)}>
                 <div className="form-group">
-                    <input className="auth-field form-control border-secondary rounded-0" onChange={e => handleInputChange(e)} value={inputs.username} name="username" type="text" placeholder="username" aria-label="username" />
+                    <input className="auth-field form-control border-secondary rounded-0" onChange={e => handleInputChange(e)} value={inputs.username} name="username" type="text" placeholder="email address" aria-label="username" />
                 </div>
                 <div className="form-group">
                     <input className="auth-field form-control border-secondary rounded-0" onChange={e => handleInputChange(e)} value={inputs.password} name="password" type="password" placeholder="password" aria-label="password" />
+                    
+                    {/* requirements toggler */}
+                    <button style={{ padding: 0 }} className="navbar-toggler password-requirements-toggler" type="button" data-toggle="collapse" data-target="#passwordRequirements" aria-controls="passwordRequirements" aria-expanded="false" aria-label="Toggle password requirements">
+                        <span className="pw-validation">password requirements</span>
+                    </button>
+                    {/* password requirements */}
+                    <ul className="collapse" id="passwordRequirements" style={{ fontSize: '.8rem' }}>
+                        <li>At least 12 characters</li>
+                        <li>At least one uppercase letter</li>
+                        <li>At least one lowercase letter</li>
+                        <li>At least one number 0-9</li>
+                        <li>At least one of the following special characters: ! @ # $ % ^ & *</li>
+                    </ul>
                 </div>
                 <div className="form-group">
                     <input className="auth-field form-control border-secondary rounded-0" onChange={e => handleInputChange(e)} value={inputs.confirmPassword} name="confirmPassword" type="password" placeholder="re-enter password" aria-label="confirmPassword" />
+                    {/* display message if passwords match */}
+                    {validate.match(inputs.password, inputs.confirmPassword) ? <p className="pw-validation" style={{ color: '#DC143C' }}>passwords match!</p> : null}
                 </div>
                 <Button type="submit">Go</Button>  
                 <p className="small-text">
--- client/src/utils/validate.js
+++ client/src/utils/validate.js
@@ -7,5 +7,15 @@ export default {
     },
     date(input) {
         return input !== '2000-01-01T00:00:00-07:00';
-    }
+    },
+    email(input) {
+        return input.match(/\S+@\S+\.\S+/) !== null;
+    },
+    password(input) {
+        return input.match(/^^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{12,}$/) !== null;
+    },
+    match(first, second) {
+        if (first === '' || second === '') return false;
+        return first === second;
+    },
 }

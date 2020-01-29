d8ab8293fa484b0e318de3ceea5d00d797ef9d57
zoe-gonzales <zoegonzales03@gmail.com>
2020-01-28 17:46:38 -0700

    troubleshoot path to static files

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func main() {
 	}
 
 	// static files
-	r.PathPrefix("/app").Handler(http.StripPrefix("/client", http.FileServer(http.Dir("./client"))))
+	r.PathPrefix("/").Handler(http.StripPrefix("/web", http.FileServer(http.Dir("./web"))))
 	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":"+port, r))
